ae2bcacb13e15c82a97198429688f028a4b45e60
robmikh <rob.mikh@outlook.com>
2021-12-18 04:29:30 -0800

    refactor encoder

---
+++ src/encoder.rs
@@ -0,0 +1,272 @@
+use std::{
+    fs::File,
+    path::Path,
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+    thread::JoinHandle,
+    time::Duration,
+};
+
+use gif::Repeat;
+use robmikh_common::universal::d3d::{create_direct3d_device, get_d3d_interface_from_object};
+use windows::{
+    core::{Handle, Result},
+    Graphics::{
+        Capture::{Direct3D11CaptureFrame, GraphicsCaptureItem},
+        SizeInt32,
+    },
+    Win32::{
+        Foundation::PWSTR,
+        Graphics::{
+            Direct3D11::{
+                ID3D11Device, ID3D11DeviceContext, ID3D11Texture1D, ID3D11Texture2D,
+                D3D11_BIND_SHADER_RESOURCE, D3D11_SUBRESOURCE_DATA, D3D11_TEXTURE1D_DESC,
+                D3D11_USAGE_DEFAULT,
+            },
+            Dxgi::Common::DXGI_FORMAT_R8G8B8A8_UINT,
+        },
+        System::Threading::{CreateEventW, SetEvent, WaitForSingleObject, WAIT_OBJECT_0},
+    },
+};
+
+use crate::{
+    capture::{CaptureFrameGenerator, CaptureFrameGeneratorSession},
+    diff::TextureDiffer,
+    handle::AutoCloseHandle,
+    lut::PaletteIndexLUT,
+    quantizer::ColorQuantizer,
+};
+
+pub struct GifEncoder {
+    _d3d_device: ID3D11Device,
+    _d3d_context: ID3D11DeviceContext,
+    _palette_texture: ID3D11Texture1D,
+    capture_session: CaptureFrameGeneratorSession,
+    should_exit: Arc<AtomicBool>,
+    start_event: AutoCloseHandle,
+    encoder_thread: JoinHandle<Result<()>>,
+    started: AtomicBool,
+}
+
+const INFINITE: u32 = 0xFFFFFFFF;
+
+impl GifEncoder {
+    pub fn new<P: AsRef<Path>>(
+        d3d_device: &ID3D11Device,
+        palette: &[u8],
+        capture_item: GraphicsCaptureItem,
+        capture_size: SizeInt32,
+        path: P,
+    ) -> Result<Self> {
+        let d3d_context = unsafe {
+            let mut d3d_context = None;
+            d3d_device.GetImmediateContext(&mut d3d_context);
+            d3d_context.unwrap()
+        };
+        let device = create_direct3d_device(d3d_device)?;
+
+        // Create the palette buffer
+        let palette_texture = {
+            let desc = D3D11_TEXTURE1D_DESC {
+                Width: 256,
+                MipLevels: 1,
+                ArraySize: 1,
+                Format: DXGI_FORMAT_R8G8B8A8_UINT,
+                Usage: D3D11_USAGE_DEFAULT,
+                BindFlags: D3D11_BIND_SHADER_RESOURCE.0,
+                ..Default::default()
+            };
+            // TODO: pSysMem shouldn't be *mut _
+            let mut palette_with_alpha = {
+                let mut palette_with_alpha: Vec<u8> = Vec::with_capacity(256 * 4);
+                for chunk in palette.chunks(3) {
+                    palette_with_alpha.push(chunk[0]);
+                    palette_with_alpha.push(chunk[1]);
+                    palette_with_alpha.push(chunk[2]);
+                    palette_with_alpha.push(255);
+                }
+                palette_with_alpha
+            };
+            let subresource_data = D3D11_SUBRESOURCE_DATA {
+                pSysMem: palette_with_alpha.as_mut_ptr() as *mut _,
+                ..Default::default()
+            };
+            unsafe { d3d_device.CreateTexture1D(&desc, &subresource_data)? }
+        };
+
+        // Create a 3d texture for our LUT
+        let lut = PaletteIndexLUT::new(&d3d_device, &d3d_context, &palette_texture)?;
+
+        // Create our color quantizer
+        let quantizer = ColorQuantizer::new(&d3d_device, &d3d_context, lut, capture_size)?;
+
+        // Create our differ
+        let mut differ = TextureDiffer::new(&d3d_device, &d3d_context, capture_size)?;
+
+        // Setup capture
+        let mut frame_generator =
+            CaptureFrameGenerator::new(device, capture_item, capture_size, 2)?;
+        let capture_session = frame_generator.session();
+
+        // Setup encoder thread
+        let start_event = unsafe {
+            let start_event =
+                CreateEventW(std::ptr::null(), true, false, PWSTR(std::ptr::null_mut())).ok()?;
+            AutoCloseHandle(start_event)
+        };
+        let should_exit = Arc::new(AtomicBool::new(false));
+        let encoder_thread = std::thread::spawn({
+            let should_exit = should_exit.clone();
+            let start_event = start_event.0;
+            let path = path.as_ref().to_owned();
+            let palette: Vec<u8> = palette.iter().map(|x| *x).collect();
+            move || -> Result<()> {
+                assert!(unsafe { WaitForSingleObject(start_event, INFINITE) } == WAIT_OBJECT_0);
+
+                // Setup the gif encoder
+                let mut image = File::create(path).unwrap();
+                let mut encoder = gif::Encoder::new(
+                    &mut image,
+                    capture_size.Width as u16,
+                    capture_size.Height as u16,
+                    &palette,
+                )
+                .unwrap();
+                encoder.set_repeat(Repeat::Infinite).unwrap();
+
+                let mut last_timestamp = None;
+                let mut process_frame = |frame: Direct3D11CaptureFrame| -> Result<()> {
+                    let (bytes, rect) = {
+                        let frame_texture: ID3D11Texture2D =
+                            get_d3d_interface_from_object(&frame.Surface()?)?;
+                        let bytes = quantizer.quantize(&frame_texture)?;
+
+                        let rect = differ.process_frame(&frame_texture)?;
+                        //println!("{:?}", rect);
+
+                        (bytes, rect)
+                    };
+
+                    // If there's no change, don't bother
+                    if let Some(mut rect) = rect {
+                        // Inflate our rect to eliminate artifacts
+                        let inflate_amount = 1;
+                        let left = rect.left as i32 - inflate_amount;
+                        let top = rect.top as i32 - inflate_amount;
+                        let right = rect.right as i32 + inflate_amount;
+                        let bottom = rect.bottom as i32 + inflate_amount;
+                        //println!("{:?}", rect);
+                        rect.left = left.max(0) as u32;
+                        rect.top = top.max(0) as u32;
+                        rect.right = right.min(capture_size.Width) as u32;
+                        rect.bottom = bottom.min(capture_size.Height) as u32;
+                        //println!("{:?}", rect);
+                        //println!("");
+
+                        // Build our gif frame
+                        let width = rect.width();
+                        let height = rect.height();
+                        let bytes = if width == capture_size.Width as u32
+                            && height == capture_size.Height as u32
+                        {
+                            bytes
+                        } else {
+                            let mut new_bytes = vec![0u8; (width * height) as usize];
+                            for j in 0..height {
+                                let dest_start = (j * width) as usize;
+                                let dest_end = dest_start + width as usize;
+                                let src_start = (((j + rect.top) * capture_size.Width as u32)
+                                    + rect.left)
+                                    as usize;
+                                let src_end = src_start + width as usize;
+                                new_bytes[dest_start..dest_end]
+                                    .copy_from_slice(&bytes[src_start..src_end]);
+                            }
+                            new_bytes
+                        };
+                        //println!("{} x {} ({}) vs {}", width, height, width * height, bytes.len());
+                        let mut gif_frame = gif::Frame::from_indexed_pixels(
+                            width as u16,
+                            height as u16,
+                            &bytes,
+                            None,
+                        );
+                        gif_frame.left = rect.left as u16;
+                        gif_frame.top = rect.top as u16;
+                        let timestamp: Duration = if last_timestamp.is_none() {
+                            let timestamp = frame.SystemRelativeTime()?;
+                            timestamp
+                        } else {
+                            last_timestamp.unwrap()
+                        }
+                        .into();
+                        let current_timestamp: Duration = {
+                            let current_timestamp = frame.SystemRelativeTime()?;
+                            last_timestamp = Some(current_timestamp);
+                            current_timestamp
+                        }
+                        .into();
+                        let frame_delay = current_timestamp - timestamp;
+                        //println!("delay: {}", frame_delay.as_millis());
+                        gif_frame.delay = (frame_delay.as_millis() / 10) as u16;
+
+                        // Write our frame to disk
+                        encoder.write_frame(&gif_frame).unwrap();
+                    }
+
+                    Ok(())
+                };
+
+                loop {
+                    if should_exit.load(Ordering::SeqCst) == true {
+                        while let Some(frame) = frame_generator.try_get_next_frame()? {
+                            process_frame(frame)?;
+                        }
+                        break;
+                    }
+                    if let Some(frame) = frame_generator.wait_for_next_frame()? {
+                        process_frame(frame)?;
+                    } else {
+                        break;
+                    }
+                }
+                Ok(())
+            }
+        });
+        Ok(Self {
+            _d3d_device: d3d_device.clone(),
+            _d3d_context: d3d_context,
+            _palette_texture: palette_texture,
+            capture_session,
+            should_exit,
+            start_event,
+            encoder_thread,
+            started: AtomicBool::new(false),
+        })
+    }
+
+    pub fn start(&mut self) -> Result<()> {
+        if self
+            .started
+            .compare_exchange(false, true, Ordering::SeqCst, Ordering::SeqCst)
+            .is_ok()
+        {
+            self.capture_session.start()?;
+            unsafe {
+                SetEvent(&self.start_event.0);
+            }
+        }
+        Ok(())
+    }
+
+    pub fn stop(self) -> Result<()> {
+        self.capture_session.stop()?;
+        self.should_exit
+            .compare_exchange(false, true, Ordering::SeqCst, Ordering::SeqCst)
+            .unwrap();
+        self.encoder_thread.join().unwrap()?;
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,28 @@
 mod capture;
 mod d3d;
 mod diff;
+mod encoder;
 mod handle;
 mod hotkey;
 mod lut;
 mod palette;
 mod quantizer;
 
-use std::{
-    fs::File,
-    sync::{
-        atomic::{AtomicBool, Ordering},
-        Arc,
-    },
-    time::Duration,
-};
-
-use gif::Repeat;
 use robmikh_common::{
     desktop::{
         capture::create_capture_item_for_monitor,
         dispatcher_queue::DispatcherQueueControllerExtensions,
         displays::get_display_handle_from_index,
     },
-    universal::d3d::{create_d3d_device, create_direct3d_device, get_d3d_interface_from_object},
+    universal::d3d::create_d3d_device,
 };
 use windows::{
-    core::{Handle, Result},
-    Graphics::{Capture::Direct3D11CaptureFrame, SizeInt32},
+    core::Result,
+    Graphics::SizeInt32,
     System::{DispatcherQueueController, VirtualKey},
     Win32::{
-        Foundation::{HWND, PWSTR},
-        Graphics::{
-            Direct3D11::{
-                ID3D11Texture2D, D3D11_BIND_SHADER_RESOURCE, D3D11_SUBRESOURCE_DATA,
-                D3D11_TEXTURE1D_DESC, D3D11_USAGE_DEFAULT,
-            },
-            Dxgi::Common::DXGI_FORMAT_R8G8B8A8_UINT,
-        },
-        System::{
-            Threading::{CreateEventW, SetEvent, WaitForSingleObject, WAIT_OBJECT_0},
-            WinRT::{RoInitialize, RO_INIT_MULTITHREADED},
-        },
+        Foundation::HWND,
+        System::WinRT::{RoInitialize, RO_INIT_MULTITHREADED},
         UI::{
             Input::KeyboardAndMouse::{MOD_CONTROL, MOD_SHIFT},
             WindowsAndMessaging::{DispatchMessageW, GetMessageW, MSG, WM_HOTKEY},
@@ -49,12 +30,7 @@ use windows::{
     },
 };
 
-use crate::{
-    capture::CaptureFrameGenerator, diff::TextureDiffer, handle::AutoCloseHandle, hotkey::HotKey,
-    lut::PaletteIndexLUT, palette::DEFAULT_PALETTE, quantizer::ColorQuantizer,
-};
-
-const INFINITE: u32 = 0xFFFFFFFF;
+use crate::{encoder::GifEncoder, hotkey::HotKey, palette::DEFAULT_PALETTE};
 
 fn main() -> Result<()> {
     unsafe {
@@ -74,12 +50,6 @@ fn main() -> Result<()> {
 
     // Init d3d11
     let d3d_device = create_d3d_device()?;
-    let d3d_context = unsafe {
-        let mut d3d_context = None;
-        d3d_device.GetImmediateContext(&mut d3d_context);
-        d3d_context.unwrap()
-    };
-    let device = create_direct3d_device(&d3d_device)?;
 
     // We're only going to capture part of the screen
     let capture_size = SizeInt32 {
@@ -89,167 +59,15 @@ fn main() -> Result<()> {
 
     // Create our palette
     let palette = &DEFAULT_PALETTE;
-    let mut palette_with_alpha = {
-        let mut palette_with_alpha: Vec<u8> = Vec::with_capacity(256 * 4);
-        for chunk in palette.chunks(3) {
-            palette_with_alpha.push(chunk[0]);
-            palette_with_alpha.push(chunk[1]);
-            palette_with_alpha.push(chunk[2]);
-            palette_with_alpha.push(255);
-        }
-        palette_with_alpha
-    };
 
-    // Create the palette buffer
-    let palette_texture = {
-        let desc = D3D11_TEXTURE1D_DESC {
-            Width: 256,
-            MipLevels: 1,
-            ArraySize: 1,
-            Format: DXGI_FORMAT_R8G8B8A8_UINT,
-            Usage: D3D11_USAGE_DEFAULT,
-            BindFlags: D3D11_BIND_SHADER_RESOURCE.0,
-            ..Default::default()
-        };
-        // TODO: pSysMem shouldn't be *mut _
-        let subresource_data = D3D11_SUBRESOURCE_DATA {
-            pSysMem: palette_with_alpha.as_mut_ptr() as *mut _,
-            ..Default::default()
-        };
-        unsafe { d3d_device.CreateTexture1D(&desc, &subresource_data)? }
-    };
-
-    // Create a 3d texture for our LUT
-    let lut = PaletteIndexLUT::new(&d3d_device, &d3d_context, &palette_texture)?;
-
-    // Create our color quantizer
-    let quantizer = ColorQuantizer::new(&d3d_device, &d3d_context, lut, capture_size)?;
-
-    // Create our differ
-    let mut differ = TextureDiffer::new(&d3d_device, &d3d_context, capture_size)?;
-
-    // Setup capture
-    let mut frame_generator = CaptureFrameGenerator::new(device, capture_item, capture_size, 2)?;
-    let capture_session = frame_generator.session();
-
-    // Setup encoder thread
-    let start_event = unsafe {
-        let start_event =
-            CreateEventW(std::ptr::null(), true, false, PWSTR(std::ptr::null_mut())).ok()?;
-        AutoCloseHandle(start_event)
-    };
-    let should_exit = Arc::new(AtomicBool::new(false));
-    let encoder_thread = std::thread::spawn({
-        let should_exit = should_exit.clone();
-        let start_event = start_event.0;
-        move || -> Result<()> {
-            assert!(unsafe { WaitForSingleObject(start_event, INFINITE) } == WAIT_OBJECT_0);
-
-            // Setup the gif encoder
-            let mut image = File::create("recording.gif").unwrap();
-            let mut encoder = gif::Encoder::new(
-                &mut image,
-                capture_size.Width as u16,
-                capture_size.Height as u16,
-                palette,
-            )
-            .unwrap();
-            encoder.set_repeat(Repeat::Infinite).unwrap();
-
-            let mut last_timestamp = None;
-            let mut process_frame = |frame: Direct3D11CaptureFrame| -> Result<()> {
-                let (bytes, rect) = {
-                    let frame_texture: ID3D11Texture2D =
-                        get_d3d_interface_from_object(&frame.Surface()?)?;
-                    let bytes = quantizer.quantize(&frame_texture)?;
-
-                    let rect = differ.process_frame(&frame_texture)?;
-                    //println!("{:?}", rect);
-
-                    (bytes, rect)
-                };
-
-                // If there's no change, don't bother
-                if let Some(mut rect) = rect {
-                    // Inflate our rect to eliminate artifacts
-                    let inflate_amount = 1;
-                    let left = rect.left as i32 - inflate_amount;
-                    let top = rect.top as i32 - inflate_amount;
-                    let right = rect.right as i32 + inflate_amount;
-                    let bottom = rect.bottom as i32 + inflate_amount;
-                    //println!("{:?}", rect);
-                    rect.left = left.max(0) as u32;
-                    rect.top = top.max(0) as u32;
-                    rect.right = right.min(capture_size.Width) as u32;
-                    rect.bottom = bottom.min(capture_size.Height) as u32;
-                    //println!("{:?}", rect);
-                    //println!("");
-
-                    // Build our gif frame
-                    let width = rect.width();
-                    let height = rect.height();
-                    let bytes = if width == capture_size.Width as u32
-                        && height == capture_size.Height as u32
-                    {
-                        bytes
-                    } else {
-                        let mut new_bytes = vec![0u8; (width * height) as usize];
-                        for j in 0..height {
-                            let dest_start = (j * width) as usize;
-                            let dest_end = dest_start + width as usize;
-                            let src_start =
-                                (((j + rect.top) * capture_size.Width as u32) + rect.left) as usize;
-                            let src_end = src_start + width as usize;
-                            new_bytes[dest_start..dest_end]
-                                .copy_from_slice(&bytes[src_start..src_end]);
-                        }
-                        new_bytes
-                    };
-                    //println!("{} x {} ({}) vs {}", width, height, width * height, bytes.len());
-                    let mut gif_frame =
-                        gif::Frame::from_indexed_pixels(width as u16, height as u16, &bytes, None);
-                    gif_frame.left = rect.left as u16;
-                    gif_frame.top = rect.top as u16;
-                    let timestamp: Duration = if last_timestamp.is_none() {
-                        let timestamp = frame.SystemRelativeTime()?;
-                        timestamp
-                    } else {
-                        last_timestamp.unwrap()
-                    }
-                    .into();
-                    let current_timestamp: Duration = {
-                        let current_timestamp = frame.SystemRelativeTime()?;
-                        last_timestamp = Some(current_timestamp);
-                        current_timestamp
-                    }
-                    .into();
-                    let frame_delay = current_timestamp - timestamp;
-                    //println!("delay: {}", frame_delay.as_millis());
-                    gif_frame.delay = (frame_delay.as_millis() / 10) as u16;
-
-                    // Write our frame to disk
-                    encoder.write_frame(&gif_frame).unwrap();
-                }
-
-                Ok(())
-            };
-
-            loop {
-                if should_exit.load(Ordering::SeqCst) == true {
-                    while let Some(frame) = frame_generator.try_get_next_frame()? {
-                        process_frame(frame)?;
-                    }
-                    break;
-                }
-                if let Some(frame) = frame_generator.wait_for_next_frame()? {
-                    process_frame(frame)?;
-                } else {
-                    break;
-                }
-            }
-            Ok(())
-        }
-    });
+    // Create our encoder
+    let mut encoder = GifEncoder::new(
+        &d3d_device,
+        palette,
+        capture_item,
+        capture_size,
+        "recording.gif",
+    )?;
 
     // Record
     let mut is_recording = false;
@@ -257,21 +75,14 @@ fn main() -> Result<()> {
         Ok(if !is_recording {
             is_recording = true;
             println!("Starting recording...");
-            capture_session.start()?;
-            unsafe {
-                SetEvent(&start_event.0);
-            }
+            encoder.start()?;
             false
         } else {
             true
         })
     })?;
     println!("Stopping recording...");
-    capture_session.stop()?;
-    should_exit
-        .compare_exchange(false, true, Ordering::SeqCst, Ordering::SeqCst)
-        .unwrap();
-    encoder_thread.join().unwrap()?;
+    encoder.stop()?;
 
     Ok(())
 }
